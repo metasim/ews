a677167fd40d0edafb5eb759f5ec0993379dbfa7
Simeon H.K. Fitch <simeon.fitch@mseedsoft.com>
2010-02-15 12:07:55 -0500

    turned off "keepfiles" and "verbose"

--- docs/design/libs/plantuml.sh
+++ docs/design/libs/plantuml.sh
@@ -3,4 +3,5 @@
 # Wherever we are, use parent dir to this script as working dir.
 cd `dirname $0`/..
 
-exec java -jar libs/plantuml.jar -config libs/plantuml.cfg -v -keepfiles $*
+#exec java -jar libs/plantuml.jar -config libs/plantuml.cfg -v -keepfiles $*
+exec java -jar libs/plantuml.jar -config libs/plantuml.cfg  $*
