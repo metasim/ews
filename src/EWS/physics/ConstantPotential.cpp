a2b005aaa95c27eef19b00a01c6548473ef7e588
bhocking <ben.hocking@mseedsoft.com>
2010-02-24 16:56:24 -0500

    Added ConstantPotential

---
+++ src/EWS/physics/ConstantPotential.cpp
@@ -0,0 +1,11 @@
+/*
+ *  ConstantPotential.cpp
+ *  EWS
+ *
+ *  Created by Ben Hocking on 2/24/10.
+ *  Copyright 2010 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "ConstantPotential.h"
+
