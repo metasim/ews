b9859a84d45b5915c78361f598dcc016ae1904e3
Ashlie B. Hocking <ben.hocking@mseedsoft.com>
2010-03-17 09:05:40 -0400

    Improved install process for Windows a bit

--- src/EWS/make.bat
+++ src/EWS/make.bat
@@ -1,6 +1,10 @@
 rmdir /s /q build.vs
 mkdir build.vs
-REM copy ..\..\..\..\OpenSceneGraph-2.8.2\bin\osg55-osgd.dll build.vs\osg55-osg.dll
+mkdir build.vs\Debug
+copy ..\..\..\..\OpenSceneGraph-2.8.2\bin\bin\*d.dll build.vs\Debug
+mkdir build.vs\Release
+copy ..\..\..\..\OpenSceneGraph-2.8.2\bin\bin\*.dll build.vs\Release
+del /Q build.vs\Release\*d.dll
 cd build.vs
 cmake -G"Visual Studio 9 2008" ..
 cd ..
